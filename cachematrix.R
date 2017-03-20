d3f672e75a4ffee6ae23eb9a26f77f69dc78700a
bolobritches <sean.p.mcgrew@gmail.com>
2017-03-19 22:49:01 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions enable caching the inverse of a matrix
 
-## Write a short comment describing this function
+## The first step is to create the matrix that can cache its inverse
+## essentially, creates an object with matrix and its inverse as data structures
+## and operators to set the value and inverse, and return them
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv<-NULL
+	set<-function(y) {
+		x<<-y
+		inv<<-NULL
+	}
+	get<-function() x
+	setinv<-function(solve) inv<<-solve
+	getinv<-function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The second function gets the inverse if already created
+## otherwise calculates the inverse and sets it in the object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      inv<-x$getinv()
+	if(!is.null(inv)) {
+		message("getting inverse from cached value")
+	      return(inv)
+	}
+	mat<-x$get()
+	inv<-solve(mat)
+        x$setinv(inv)
+	inv
 }
+                
+## tried to follow example closely. But not sure line 14 is right, or if so, why we can't use the setinv operator instead of inv<solve(mat) in line 30.
